e0c4b0ed0853e6f2c1cf1019937073842c703ae1
Anastasiia Lytvynova <anastasiia.lytvynova@artcom-venture.de>
2022-04-24 14:56:36 +0200

    formatting2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,24 @@
-import './app.css';
-import SearchBar from '../search-bar/search-bar';
-import Books from '../../front-end/books';
-import { useEffect, useState } from 'react';
+import "./app.css";
+import SearchBar from "../search-bar/search-bar";
+import Books from "../../front-end/books";
+import { useEffect, useState } from "react";
 
 function App() {
-    const [booksArray, setBooksArray] = useState([]);
-    useEffect(() => {
-        new Books().askListBooks().then(books => {
-            setBooksArray(books);
-        });
-    }, []);
+  const [booksArray, setBooksArray] = useState([]);
+  useEffect(() => {
+    new Books().askListBooks().then((books) => {
+      setBooksArray(books);
+    });
+  }, []);
 
-    return(
-        <div>
-        <div className="app">
-            <h1>Find your favorite books</h1>
-        </div>
-        <SearchBar books={booksArray}/>
-        </div>
-    )
+  return (
+    <div>
+      <div className="app">
+        <h1>Find your favorite books</h1>
+      </div>
+      <SearchBar books={booksArray} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/search-autocomplete/search-autocomplete.js
+++ src/components/search-autocomplete/search-autocomplete.js
@@ -1,64 +1,83 @@
-import './search-autocomplete.css'
+import "./search-autocomplete.css";
 
-const SearchAutocomplete = ({foundByBook, foundByAuthor, input, handleClick}) => {
-
-    function getHighlightedText(text) {
-        const parts = text.split(new RegExp(`(${input})`, 'gi'));
-        return <span>{parts.map(part => part.toLowerCase() === input.toLowerCase() ? <b>{part}</b> : part)}</span>;
-    }
-    const foundBooks = foundByBook.length ? <div className="autocomplete__section">
-    <div 
-    className="autocomplete__title">Books</div>
-    <div 
-    className="autocomplete__items">
-    {foundByBook.map(function (el, index) { 
-    return <div 
-    key={index}
-    className="autocomplete__item"
-    onClick={() => handleClick(el.title)}
-    >
-        <div 
-        key={Math.random()*100 + el.title}
-        className="autocomplete__item-title">{getHighlightedText(el.title)}</div>
-        <div 
-        key={Math.random()*300 + el.title}
-        className="autocomplete__item-subtext">{el.author} - Published in {el.year}</div>
-    </div>})}
-    </div>
-    </div> : null;
-
-    const foundAuthors = foundByAuthor.length ? <div className="autocomplete__section">
-    <div className="autocomplete__title">Authors</div>
-    <div className="autocomplete__items">
-    {foundByAuthor.map(function (el, index) {
-    return <div 
-    key={index}
-    className="autocomplete__item"
-    onClick={() => handleClick(el.author)}
-    >
-        <div className="autocomplete__item-title">{getHighlightedText(el.author)}</div>
-        <div className="autocomplete__item-subtext">Wrote {el.title}</div>
-    </div>;
-    })}
-    </div>
-    </div> : null;
-
-    return foundByBook.length || foundByAuthor.length ? (
-        <div className="autocomplete">
-          <div className="autocomplete__wrapper">
-              {foundBooks}
-              {foundAuthors}
+const SearchAutocomplete = ({
+  foundByBook,
+  foundByAuthor,
+  input,
+  handleClick,
+}) => {
+  function getHighlightedText(text) {
+    const parts = text.split(new RegExp(`(${input})`, "gi"));
+    return (
+      <span>
+        {parts.map((part) =>
+          part.toLowerCase() === input.toLowerCase() ? <b>{part}</b> : part
+        )}
+      </span>
+    );
+  }
+  const foundBooks = foundByBook.length ? (
+    <div className="autocomplete__section">
+      <div className="autocomplete__title">Books</div>
+      <div className="autocomplete__items">
+        {foundByBook.map(function (el, index) {
+          return (
+            <div
+              key={index}
+              className="autocomplete__item"
+              onClick={() => handleClick(el.title)}
+            >
+              <div className="autocomplete__item-title">
+                {getHighlightedText(el.title)}
+              </div>
+              <div className="autocomplete__item-subtext">
+                {el.author} - Published in {el.year}
+              </div>
             </div>
-        </div>)
-        : (
-        <div className="autocomplete">
-            <div className="autocomplete__wrapper">
-                <div className="no-books">
-                    <div className="no-books__title">No matches</div>
-                </div>
+          );
+        })}
+      </div>
+    </div>
+  ) : null;
+
+  const foundAuthors = foundByAuthor.length ? (
+    <div className="autocomplete__section">
+      <div className="autocomplete__title">Authors</div>
+      <div className="autocomplete__items">
+        {foundByAuthor.map(function (el, index) {
+          return (
+            <div
+              key={index}
+              className="autocomplete__item"
+              onClick={() => handleClick(el.author)}
+            >
+              <div className="autocomplete__item-title">
+                {getHighlightedText(el.author)}
+              </div>
+              <div className="autocomplete__item-subtext">Wrote {el.title}</div>
             </div>
+          );
+        })}
+      </div>
+    </div>
+  ) : null;
+
+  return foundByBook.length || foundByAuthor.length ? (
+    <div className="autocomplete">
+      <div className="autocomplete__wrapper">
+        {foundBooks}
+        {foundAuthors}
+      </div>
+    </div>
+  ) : (
+    <div className="autocomplete">
+      <div className="autocomplete__wrapper">
+        <div className="no-books">
+          <div className="no-books__title">No matches</div>
         </div>
-        );
-}
+      </div>
+    </div>
+  );
+};
 
 export default SearchAutocomplete;
